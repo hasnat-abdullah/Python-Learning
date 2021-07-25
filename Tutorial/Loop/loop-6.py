d5d3b1042674b7030da5888df3e7028025b46408
Hasnat_strativ <hasnat@strativ.se>
2021-07-25 12:13:49 +0600

    hw & nested loop- modified

--- Tutorial/Loop/loop-6.py
+++ Tutorial/Loop/loop-6.py
@@ -3,6 +3,10 @@ Nested loop
 '''
 
 for i in range(0,5): # Outer loop
+    print(f'{i}->', end='')
+
     for j in range(0,5): # Inner loop
         print(j, end='')
     print() # To print new line
+
+print("the program is end.")
